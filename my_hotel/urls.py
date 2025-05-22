e08e6b742dc715d3453217e164fd9db81c840dc6
Fili258-oss <marinorubianobotina@gmail.com>
2025-05-22 17:48:27 -0500

    Correccion de urls

--- my_hotel/urls.py
+++ my_hotel/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))  # Páginas principales
+    path('', include('core.urls')),
+    path('', include('accounts.urls')),
+    # Páginas principales
     
 ]
